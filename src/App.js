00f91985e39d6cb0bf5bde933ce212e94f7a0234
colin1717 <colin1717@mac.com>
2016-11-21 11:33:52 -0600

    feedpicker buttons in component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css'
 import Header from './Header';
+import FeedPicker from './FeedPicker';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <FeedPicker />
       </div>
+
     )
   }
 }
