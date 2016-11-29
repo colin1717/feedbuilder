26c54250caeba7a0562d163075d695f1a3ed75a7
colin1717 <colin1717@mac.com>
2016-11-29 17:00:02 -0600

    present columns now accessible in App methods

--- src/App.js
+++ src/App.js
@@ -7,6 +7,32 @@ import FileInput from './child_components/FileInput';
 var Baby = require('babyparse');
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.presentColumns = new Set();
+
+    this._passPresentColumns = function(presentColumns){
+      this.presentColumns = presentColumns;
+    }
+
+    this._addCsv = function(data) {
+      const newCsv = {
+        data
+      };
+
+      newCsv.parsed = Baby.parse(newCsv.data);
+      console.log(newCsv.parsed);
+      console.log('====this.presentColumns====');
+      console.log(this.presentColumns);
+    }
+
+    this.test = function(){
+      console.log(this.presentColumns);
+    }
+
+  }
+
   render() {
 
     const importColumns = [{id: 1, title:'Product-ExternalId', required:true}, {id:2, title:'SubmissionTime', required: false}, {id:3, title:'Title',required:false}, {id:4, title:'ReviewText', required:false}, {id:5, title:'Rating',required: false}, {id:6, title:'IpAddress', required:false}, {id:7, title:'UserEmailAddress', required:false}, {id:8, title:'ReviewerLocation', required:false}, {id:9, title:'DisplayLocale', require:false}, {id:10, title:'User-ExternalId', required: false}];
@@ -15,25 +41,12 @@ class App extends Component {
       <div>
         <Header />
         <FeedPicker />
-        <ColumnTable columns={ importColumns } passPresentColumns={this._passPresentColumns}/>
+        <ColumnTable columns={ importColumns } passPresentColumns={this._passPresentColumns.bind(this)}/>
         <FileInput addCsv={this._addCsv.bind(this)}/>
+        <button onClick={this.test.bind(this)}></button>
       </div>
 
     )
-  }
-
-  _addCsv(data) {
-    const newCsv = {
-      data
-    };
-
-    newCsv.parsed = Baby.parse(newCsv.data);
-    console.log(newCsv.parsed);
-  }
-
-  _passPresentColumns(presentColumns){
-    console.log('=====presentColumns=====');
-    console.log(presentColumns);
   }
 
 }
